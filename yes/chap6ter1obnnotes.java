e9b9b64550e1ec795d0de22a7cffaa5d93f2ccf7
smkaszuba <smkaszuba2@stu.naperville203.org>
2022-08-23 14:20:02 -0500

    yes

---
+++ yes/chap6ter1obnnotes.java
@@ -0,0 +1,23 @@
+// code 4 notes
+import java.util.Scanner;
+import java.util.Random;
+public class chap6ter1obnnotes{
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        // JAVA DATA TPYES
+        int x = 16777216,y,z;
+        String word = "fart";
+        boolean True = true;
+        char letter = 'b';
+        float afdsfds = 3;
+        double alslskd = 1.23122;
+        short num = 6;
+        long fart = 32342423;
+        
+        String wofard = new String("dookiedookiedookiedookie");
+        Integer number = new Integer(12);
+        Double afdsjfdslkj = new Double(32.12);
+        
+        System.out.print(wofard);
+    }
+}
