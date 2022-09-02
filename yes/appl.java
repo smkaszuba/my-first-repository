aa0105e56187c406bd75dc9e7d19b7e696a88784
smkaszuba <smkaszuba2@stu.naperville203.org>
2022-09-02 13:50:59 -0500

    f

---
+++ yes/appl.java
@@ -0,0 +1,15 @@
+// Mitchell Kaszuba
+// 9/1/22
+// random phone number maker
+// makes random phones numbers
+// import math
+import java.applet.*;
+import java.awt.*;
+
+public class appl extends Applet
+{
+  public static void main(Graphics page) 
+  {
+    page.drawRect(1,1,1,1);
+  }
+}
--- yes/randomPhoneNumber.java
+++ yes/randomPhoneNumber.java
@@ -10,20 +10,20 @@ public class randomPhoneNumber
   public static void main(String[] args) 
   {
     // number is input, phoneNumber is output
-    String phoneNumber;
+    String number, phoneNumber;
     
     // create random class
     Random random = new Random();
     
-    Double number = new Double(random.nextDouble() * 6779999999 + 1000000000);
-    
     // create a random 10 digit number using random, then converting to string
-    number = String.valueOf((double) random.nextDouble() * 6770000000L + 1000000000L);
+    number = String.valueOf((long) Math.floor(Math.random() * 6770000000L) + 1000000000L);
+
     
     // create a phone number by inserting dashes where they usually are
     phoneNumber = new String(number.substring(0 , 3) + "-" + number.substring(3 , 6) + "-" + number.substring(6 , 10));
     
     // print info to user
+    System.out.println(number);
     System.out.print(phoneNumber);
   }
 }
